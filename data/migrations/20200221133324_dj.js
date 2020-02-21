9cf682c7d84ee2b3ea7f82a3797d2e67eded0474
aaamg <andrewmanuelgarcia@gmail.com>
2020-02-21 13:41:53 -0500

    added test seed

--- data/migrations/20200221133324_dj.js
+++ data/migrations/20200221133324_dj.js
@@ -1,6 +1,11 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('dj-login', tbl => {
+        // creates a primary key called id
+        tbl.increments();
+        tbl.text('username', 128).unique().notNullable();
+        tbl.text('password').notNullable();
+      })
 };
 
 exports.down = function(knex) {
