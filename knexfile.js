8eb0aa84e7f3bfda16a5bbb4aa458f7b3ae4d7c4
aaamg <andrewmanuelgarcia@gmail.com>
2020-02-24 19:11:42 -0500

    postgres set up and running locally

--- knexfile.js
+++ knexfile.js
@@ -2,33 +2,39 @@
 //Following this guide
 //https://dev.to/easybuoy/setting-up-a-node-api-with-postgres-and-knex-588f
 
+/* 
+Testing:
+
 const pgUser = process.env.PG_USER || 'postgres';
 const pgDb = process.env.PG_DB || 'postgres'
 const connection = `postgres://${pgUser}@localhost/${pgDb}`;
+
+*/
+
+
 module.exports = {
 
   development: {
-    // our DBMS driver
     client: 'pg',
-    // the location of our db
-    //connection: process.env.DB_URL,
-    connection: connection,
-    // necessary when using sqlite3
-    useNullAsDefault: true,
-    // generates migration files in a data/migrations/ folder
+    connection: {
+      host: 'localhost',
+      port: 5432,
+      user: 'postgres',
+      password: 'password',
+      database: 'test'
+    },
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
-  },
-  pool: {
-    min: 2,
-    max: 10,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    }
 
-  }
-  }
-};
+}};
 
 //sqlite settings for connection
 // connection: {
