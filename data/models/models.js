9e08dacabe23bb6ebe5530d78686724a03e70ed2
aaamg <andrewmanuelgarcia@gmail.com>
2020-02-25 16:11:43 -0500

    added new unauthorized routes for DJ registration, login, update their info, and delete a DJ profile

--- data/models/models.js
+++ data/models/models.js
@@ -3,20 +3,53 @@ const db = require('../db-config');
 module.exports = {
     getAllDJs,
     getDJsByID,
-    addDJ
+    addDJ,
+    findBy,
+    updateDJ,
+    removeDJ
 }
 
+//Get every registered DJ's information
 function getAllDJs(){
     return db('dj-login')
 }
 
+//Get a specific DJ by their id
 function getDJsByID(id){
     return db('dj-login').where({ id });
 }
 
-//Add DJ
+//Add a new DJ to the table
 function addDJ(info) {
     return db('dj-login')
       .insert(info)
       .then(([id]) => this.get(id));
   }
+
+  /*
+  Another registration option if above does not work:
+  function register(info) {
+    return db('dj-login')
+    .insert(info, 'id')
+};
+  */
+
+  //Login for a DJ
+  function findBy(filter) {
+    return db('dj-login').where(filter);
+  }
+
+  //Update a DJs info they used at registration
+  function updateDJ(id, changes) {
+    return db('dj-login')
+      .where('id', id)
+      .update(changes)
+      .then(count => (count > 0 ? this.get(id) : null));
+  }
+
+  //completely remove a DJ
+    function removeDJ(id) {
+        return db('dj-login')
+        .where('id', id)
+        .del();
+    }
--- data/router/router.js
+++ data/router/router.js
@@ -28,7 +28,7 @@ router.get('/DJs/:id', (req, res) => {
     })
 })
 
-//POST add a DJ-- WORKS
+//POST add a DJ-- WORKS BUT READING 500 ERROR <---
 router.post('/register/DJ', (req, res) => {
     const body = req.body;
     db.addDJ(body)
@@ -37,6 +37,47 @@ router.post('/register/DJ', (req, res) => {
     })
     .catch(ress => {
         res.status(500).json({error: "POST ERROR: You are not registering a DJ properly"})
+    })
+})
+
+//POST to Login -- I THINK WORKS! 
+router.post('/login/DJ', (req, res) => {
+    // implement login
+    let {username, password} = req.body;
+    db.findBy({ username })
+    .first()
+    .then(user => {
+        res.status(200).json({ message: `Welcome ${db.username}`})
+      })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+  });
+
+  //Update DJ data -- WORKS BUT READING 500 ERROR <---
+router.put('/updateDJ/:id', (req, res) => {
+    const id = req.params.id;
+    //const {description, notes} = req.body;
+    const body = req.body;
+    db.updateDJ(id, body)
+    .then(story => {
+        res.status(200).json(story);
+      })
+      .catch(err => {
+        res.status(500).json({ error: "UPDATE ERROR: You are not getting DJ data back" });
+      });
+})
+
+//DEL -- WORKS (Currently has 1 removed)
+router.delete('/deleteDJ/:id', (req, res) => {
+    const id = req.params.id;
+    const {description, notes} = req.body;
+    db.removeDJ(id)
+    .then(story => {
+        res.status(200).json({ message: `${story} removed`})
+    })
+    .catch(ress => {
+        res.status(500).json({error: "DELETE ERROR: You are not getting any DJ data back"})
     })
 })
 
