3cdfc2dc80ecb7a1eb2380051179ba1ee3407e57
Dan Johnson <danimal.johnson@gmail.com>
2020-03-04 07:26:33 -0800

    Adds configuration for production environment.
    Adds development and production configurations in index.js.
    Production configuration requires PRODUCTION_ENVIRONMENT to be set to a value in .env file.
    Production configuration requires SSL certificate files server.key and server.cert in /cert directory.
    
    Bug fix: update error handling for endpoint /login/dj.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 require('dotenv').config();
 
+const fs = require('fs');
+const https = require('https');
 const server = require('./server.js');
 
 const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
-});
+
+// Add "PRODUCTION_ENVIRONMENT = true" in .env file
+if (process.env.PRODUCTION_ENVIRONMENT) {
+  https
+    .createServer(
+      {
+        key: fs.redFileSync('./cert/server.key'),
+        cert: fs.readFileSync('./cert/server.cert')
+      },
+      server
+    )
+    .listen(PORT, () => {
+      console.log(`\n=== Secure server listening on port ${PORT} ===\n`);
+    });
+} else {
+  server.listen(PORT, () => {
+    console.log(`\n=== Server listening on port ${PORT} ===\n`);
+  });
+}
