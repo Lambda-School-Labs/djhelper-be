94ab8ad63eaf3c08358439cec8c8939dd4be0bcb
aaamg <andrewmanuelgarcia@gmail.com>
2020-03-12 17:05:21 -0400

    added tests for GET routes (all and by ID) for event & location tables

--- data/routers/publicRouter.spec.js
+++ data/routers/publicRouter.spec.js
@@ -18,9 +18,39 @@ describe('Public Router tests', function() {
     expect(res.status).toBe(200);
     expect(res.type).toMatch(/json/i);
   });
-
+// -- Tests to see if /dj/:id route is running
   test('get /api/dj/1 should return dj with ID 1', async function() {
     const res = await request(server).get('/api/dj/1');
+    expect(res.status).toBe(200);
+    expect(res.type).toMatch(/json/i);
+  });
+});
+
+// -- Tests to see if /events route is running
+describe('Public Router tests', function() {
+  test('get /api/events should return all events', async function() {
+    const res = await request(server).get('/api/events');
+    expect(res.status).toBe(200);
+    expect(res.type).toMatch(/json/i);
+  });
+// -- Tests to see if /event/:id route is running
+  test('get /api/event/1 should return event with ID 1', async function() {
+    const res = await request(server).get('/api/event/1');
+    expect(res.status).toBe(200);
+    expect(res.type).toMatch(/json/i);
+  });
+});
+
+// -- Tests to see if /locations route is running
+describe('Public Router tests', function() {
+  test('get /api/locations should return all locations', async function() {
+    const res = await request(server).get('/api/locations');
+    expect(res.status).toBe(200);
+    expect(res.type).toMatch(/json/i);
+  });
+// -- Tests to see if /location/:id route is running
+  test('get /api/location/1 should return location with ID 1', async function() {
+    const res = await request(server).get('/api/location/1');
     expect(res.status).toBe(200);
     expect(res.type).toMatch(/json/i);
   });
