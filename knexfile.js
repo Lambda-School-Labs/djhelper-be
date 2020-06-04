11bc1d0cc845ad68d00ec3fd44a8d627a6759b79
Shya Biswas <shyabiswas@gmail.com>
2020-06-03 22:06:52 -0400

    env file setup

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      host: process.env.DB_HOSTNAME, // 'localhost',
-      port: process.env.PG_PORT, // 5432,
-      user: process.env.PG_USER, // 'postgres',
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE_NAME // 'test'
-    },
+    connection: process.env.DB_URL,
+    // connection: {
+    //   host: process.env.DB_HOSTNAME, // 'localhost',
+    //   port: process.env.DB_PORT, // 5432,
+    //   user: process.env.DB_USER, // 'postgres',
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE_NAME // 'test'
+    // },
     // generates migration files in a data/migrations/ folder
     migrations: {
       directory: './data/migrations'
