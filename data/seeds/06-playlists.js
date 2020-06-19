9c5b0b560d2324e372c2eea3259309638f742632
Mohd Raza <noorinfotechinc@gmail.com>
2020-06-18 21:39:43 -0500

    router authentication

--- data/seeds/06-playlists.js
+++ data/seeds/06-playlists.js
@@ -1,5 +1,7 @@
 const playlistData = require('../jsonData/playlists.json');
 
+console.log(playlistData.length, 'playlists being inserted');
+
 const createPlaylist = playlist => {
   return {
     event_id: playlist.event_id,
