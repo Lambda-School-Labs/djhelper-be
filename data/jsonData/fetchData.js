c355f9fc942c3e8b207ef1b7d13f92137f8804eb
Mohd Raza <noorinfotechinc@gmail.com>
2020-08-20 22:56:42 -0500

    updated fetch

--- data/jsonData/fetchData.js
+++ data/jsonData/fetchData.js
@@ -1,6 +1,14 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
+fetch('https://dj-helper-be.herokuapp.com/api/djs')
+  .then(res => res.json())
+  .then(data => {
+    console.log('working');
+    const songs = JSON.stringify(data, null, 2);
+    fs.writeFileSync('djs.json', songs, err => console.log(err));
+  });
+
 fetch('https://dj-helper-be.herokuapp.com/api/events')
   .then(res => res.json())
   .then(data => {
